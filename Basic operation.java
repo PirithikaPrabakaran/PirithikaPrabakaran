86843aea1fa51e521b1664fc69a0b4cec5a14b49
PirithikaPrabakaran <prithikaa2006@gmail.com>
2025-02-18 22:45:32 +0530

    Create Basic operation.java

---
+++ Basic operation.java
@@ -0,0 +1,7 @@
+public class Basic operation {    
+public static void main(String args[]){    
+int a= 68;    
+int b= 76;    
+System.out.println(a++ + ++a);    
+System.out.println(b++ + b++); 
+}}    
